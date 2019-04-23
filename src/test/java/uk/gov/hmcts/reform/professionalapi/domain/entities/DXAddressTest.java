f87296eecf12ef343e0717770bf5ec621f0a68af
Oozeerally <778425@cognizant.com>
2019-04-23 15:31:22 +0100

    created unit test for DXAddress entity

---
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/domain/entities/DXAddressTest.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.professionalapi.domain.entities;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class DXAddressTest {
+	
+	@Test
+	public void creates_dx_address_correctly() {
+
+        DXAddress dxAddress = new DXAddress(null, "some-number", "some-exchange");
+
+        assertThat(dxAddress.getId()).isNull();
+        assertThat(dxAddress.getDxNumber()).isEqualTo("some-number");
+        assertThat(dxAddress.getDxExchange()).isEqualTo("some-exchange");
+    }
+
+}
